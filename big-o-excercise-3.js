0c54f89ed21f30edf9857934a46cd384b5a64860
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 17:51:57 +0530

    Big O excerise 3: retrieving most recent and oldest tweet -- O(1)

---
+++ big-o-excercise-3.js
@@ -0,0 +1,8 @@
+// Build a feature that retrieves the most recent tweet and the oldest tweet
+// Find 1st, find Nth...
+const array = ["hi", "my", "teddy"];
+array[0]; // O(1), because retrieving from an array takes O(1)
+array[array.length -1]; // O(1)
+
+// time complexity: O(1)
+
