c6683ffaacd76d9c9fe1da82c6859a1a7f68cef8
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 13:21:59 +0530

    Example 2

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const large = new Array(100000).fill("nemo");
-function findNemo(array){
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === "nemo"){
-            console.log("Found NEMO"); // O(n)
-        }
-    }
-    console.log(`Call to findNemo() took ${t1 - t0} millis.`); // O(1)
+// const large = new Array(100000).fill("nemo");
+// function findNemo(array){
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] === "nemo"){
+//             console.log("Found NEMO"); // O(n)
+//         }
+//     }
+//     console.log(`Call to findNemo() took ${t1 - t0} millis.`); // O(1)
+// }
+
+// findNemo(large); // O(n + 1) = O(n)
+
+const compressFirstBox = (boxes) => {
+    console.log(boxes[0]);
 }
 
-findNemo(large); // O(n + 1) = O(n)
+compressFirstBox([1, 2, 3, 4, 5]);
