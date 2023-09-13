ec4a48f7177a58d8b37dd4d4ac83bcc0e80408af
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 13:28:23 +0530

    Big O: O(n) linear time

--- big-o-excercise-1.js
+++ big-o-excercise-1.js
@@ -1,12 +1,18 @@
 // What is the Big O of the function given below?
 
 function functionfunChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
   for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
   }
-  return a;
+  return a; // O(1)
 }
+
+// Big O:
+// O(1 + 1 + n + n + n + 1)
+// O(3 + 3n)
+// O(3n)
+// O(n) --> linear time
