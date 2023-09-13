6c08892499d78ef502600a8c260ec414458111a7
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 16:46:46 +0530

    Slightly improved findNemo for certain cases

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-// const large = new Array(100000).fill("nemo");
-// function findNemo(array){
-//     for(let i = 0; i < array.length; i++){
-//         if(array[i] === "nemo"){
-//             console.log("Found NEMO"); // O(n)
-//         }
-//     }
-//     console.log(`Call to findNemo() took ${t1 - t0} millis.`); // O(1)
-// }
-
-// findNemo(large); // O(n + 1) = O(n)
-
-const compressFirstBox = (boxes) => {
-    console.log(boxes[0]); // O(1)
+const nemo = ["nemo"];
+const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+const large = new Array(100).fill("nemo");
+function findNemo(array){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === "nemo"){
+            console.log("Found NEMO");
+            break;
+        }
+    }
 }
 
-compressFirstBox([1, 2, 3, 4, 5]); // Big O: O(1)
+// const compressFirstBox = (boxes) => {
+//     console.log(boxes[0]); // O(1)
+// }
+
+// compressFirstBox([1, 2, 3, 4, 5]); // Big O: O(1)
