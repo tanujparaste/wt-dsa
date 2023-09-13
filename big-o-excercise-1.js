662cd531942e4812e8dc0f4b5c4c6b593ed173f3
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 13:25:23 +0530

    Big O excercise 1

---
+++ big-o-excercise-1.js
@@ -0,0 +1,12 @@
+// What is the Big O of the function given below?
+
+function functionfunChallenge(input) {
+  let a = 10;
+  a = 50 + 3;
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction();
+    let stranger = true;
+    a++;
+  }
+  return a;
+}
