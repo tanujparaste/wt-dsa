420fe1ee9efc5e29e122ad2dfe24f818cf90bbc2
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 17:39:53 +0530

    Example 2 space complexity

--- space-complexity.js
+++ space-complexity.js
@@ -1,8 +1,21 @@
-function booooo(n) {
-  for (let i = 0; i < n.length; i++) {
-    // i = 0, is the only variable that we are creating: O(1)
-    console.log("booooo!");
-  }
+// function booooo(n) {
+//   for (let i = 0; i < n.length; i++) {
+//     // i = 0, is the only variable that we are creating: O(1)
+//     console.log("booooo!");
+//   }
+// }
+
+// booooo([1, 2, 3, 4, 5]); // O(1) --> constant space
+
+
+// Example 2: Space complexity
+
+function arrayOfHiNTimes(n){
+    let hiArray = [];
+    for(let i = 0; i < n; i++){
+        hiArray[i] = "hi";
+    }
+    return hiArray;
 }
 
-booooo([1, 2, 3, 4, 5]); // O(1) --> constant space
+console.log(arrayOfHiNTimes(6));
