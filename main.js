553f74ef4b18e97254b744c4612bac94de682fcd
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 13:17:29 +0530

    Measuring performance of findNemo with a large dataset

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const nemo = ["nemo"];
-const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+// const nemo = ["nemo"];
+// const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+const large = new Array(100000).fill("nemo");
 function findNemo(array){
     let t0 = performance.now();
     for(let i = 0; i < array.length; i++){
@@ -11,4 +12,4 @@ function findNemo(array){
     console.log(`Call to findNemo() took ${t1 - t0} millis.`); 
 }
 
-findNemo(everyone);
+findNemo(large);
