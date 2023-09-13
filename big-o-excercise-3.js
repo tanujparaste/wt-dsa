7bbd0a1b387a29c700936af4ca373aa6857067cd
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 17:56:01 +0530

    Big O excerise 3: compare dates of tweets -- O(n^2)

--- big-o-excercise-3.js
+++ big-o-excercise-3.js
@@ -1,8 +1,13 @@
 // Build a feature that retrieves the most recent tweet and the oldest tweet
 // Find 1st, find Nth...
-const array = ["hi", "my", "teddy"];
+const array = [
+  { tweet: "hi", date: 2012 },
+  { tweet: "my", date: 2014 },
+  { tweet: "teddy", date: 2018 },
+];
 array[0]; // O(1), because retrieving from an array takes O(1)
-array[array.length -1]; // O(1)
+array[array.length - 1]; // O(1)
 
 // time complexity: O(1)
 
+// compare the dates of tweets : O(n^2)
