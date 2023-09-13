a6789faf2a04130f101b6ec436d1943ae9e3891d
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 16:49:49 +0530

    Example for Big O rule 2

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const nemo = ["nemo"];
-const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
-const large = new Array(100).fill("nemo");
-function findNemo(array){
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === "nemo"){
-            console.log("Found NEMO");
-            break;
-        }
-    }
-}
+// const nemo = ["nemo"];
+// const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
+// const large = new Array(100).fill("nemo");
+// function findNemo(array){
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] === "nemo"){
+//             console.log("Found NEMO");
+//             break;
+//         }
+//     }
+// }
 
-findNemo(large); // Big O: O(n)
+// findNemo(large); // Big O: O(n)
 
 // const compressFirstBox = (boxes) => {
 //     console.log(boxes[0]); // O(1)
 // }
 
 // compressFirstBox([1, 2, 3, 4, 5]); // Big O: O(1)
+
+function printFirstItemThenFirstHalfThenSayHi100Times(items){
+    console.log(items[0]);
+    let middleIndex = Math.floor(items.length / 2);
+    let index = 0;
+    
+    while(index < middleIndex){
+      console.log(items[index]);
+      index++;
+    }
+  
+    for(let i = 0; i < 100; i++){
+      console.log("hi");
+    }
+  }
+  
+  printFirstItemThenFirstHalfThenSayHi100Times([1,2,3,4,5]);
