9221f33c2b1cf6e1c7d12160c61227339d5de698
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-13 13:19:37 +0530

    big o: O(n)

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-// const nemo = ["nemo"];
-// const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"];
 const large = new Array(100000).fill("nemo");
 function findNemo(array){
-    let t0 = performance.now();
     for(let i = 0; i < array.length; i++){
         if(array[i] === "nemo"){
-            console.log("Found NEMO");
+            console.log("Found NEMO"); // O(n)
         }
     }
-    let t1 = performance.now();
-    console.log(`Call to findNemo() took ${t1 - t0} millis.`); 
+    console.log(`Call to findNemo() took ${t1 - t0} millis.`); // O(1)
 }
 
-findNemo(large);
+findNemo(large); // O(n + 1) = O(n)
