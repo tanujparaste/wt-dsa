a35e60194673a7a808dc8ed35b6a9ef9a0dcdce5
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-14 19:52:04 +0530

    Problem statement 1

---
+++ main.js
@@ -0,0 +1,9 @@
+// Given 2 arrays, create a function that lets a user know (true/false) whether these two array contain any common items
+// For example:
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+// should return false.
+//-----------------
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+// should return true.
