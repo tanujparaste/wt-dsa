5d8f68e9c1481de5b9a20cdadb630af212c595f1
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-14 20:04:30 +0530

    Approach 1: brute force, O(m*n)

--- main.js
+++ main.js
@@ -7,3 +7,24 @@
 // const array1 = ['a', 'b', 'c', 'x'];
 // const array2 = ['z', 'y', 'x'];
 // should return true.
+
+// How to approach?
+// 2 parameters - arrays - no size limit
+// return true or false
+
+// Brute force: O(n^2) - coding may not be required
+function hasCommonItems(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+const array1 = ["a", "b", "c", "x"];
+const array2 = ["z", "y", "x"];
+
+console.log(hasCommonItems(array1, array2));
