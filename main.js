37ed809fae72ea51d18e1784b78ccc8117f1d7e7
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-09-14 20:53:32 +0530

    Cleaner code using built in apis

--- main.js
+++ main.js
@@ -26,7 +26,7 @@
 // }
 
 const array1 = ["a", "b", "c", "x"];
-const array2 = ["z", "y", "r"];
+const array2 = ["z", "y", "a"];
 
 // console.log(hasCommonItems(array1, array2));
 
@@ -40,32 +40,39 @@ const array2 = ["z", "y", "r"];
 // }
 // array2[index] === obj.properties
 
-function hasCommonItems2(arr1, arr2) {
-  // loop through first array and create object
-  // where properties === items in the array
-  const map = {};
-  for (let i = 0; i < arr1.length; i++) {
-    const item = arr1[i];
-    if (!map[item]) {
-      map[item] = true;
-    }
-  }
+// function hasCommonItems2(arr1, arr2) {
+//   // loop through first array and create object
+//   // where properties === items in the array
+//   const map = {};
+//   for (let i = 0; i < arr1.length; i++) {
+//     const item = arr1[i];
+//     if (!map[item]) {
+//       map[item] = true;
+//     }
+//   }
 
-  // console.log(map);
-  // loop through second array and check if item in
-  // second array exists on created objects
-  for (let j = 0; j < arr2.length; j++) {
-    const item = arr2[j];
-    if (map[item]) {
-      return true;
-    }
-  }
-  return false;
-}
+//   // console.log(map);
+//   // loop through second array and check if item in
+//   // second array exists on created objects
+//   for (let j = 0; j < arr2.length; j++) {
+//     const item = arr2[j];
+//     if (map[item]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-console.log(hasCommonItems2(array1, array2)); // time complexity: O(m + n) because we are not using nested loop
+// console.log(hasCommonItems2(array1, array2)); // time complexity: O(m + n) because we are not using nested loop
 
 // improving code by checking
 // how can we break the code
 // always have 2 parameter that are arrays?
 // what if one of the arg passed is a primitive or null?
+
+// cleaner code
+function hasCommonItems3(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item));
+}
+
+console.log(hasCommonItems3(array1, array2));
